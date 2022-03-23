3555a80ad82f30dd2285395f70c4c124e3e7dc2a
Patrick Valentino <72351244+ItsMe-Patrick@users.noreply.github.com>
2022-03-23 12:12:44 +0700

    Add files via upload

---
+++ CafeAPI/Controllers/FoodsController.cs
@@ -0,0 +1,35 @@
+﻿using CafeAPI.Models;
+using CafeAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CafeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodsController : ControllerBase
+    {
+        private readonly IFoodRepo _foodRepo;
+
+        public FoodsController(IFoodRepo foodRepo)
+        {
+            _foodRepo = foodRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Food>> GetBooks()
+        {
+            return await _foodRepo.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Food>> GetBooks(int id)
+        {
+            return await _foodRepo.Get(id);
+        }
+
+    }
+}
